d3b0deae07aa9a89b39ea0d36916791bf32a7125
AndroidO9 <120441498+AndroidO9@users.noreply.github.com>
2025-01-31 16:11:02 +0300

    norm

--- Assets/teleport to labarotorio.cs
+++ Assets/teleport to labarotorio.cs
@@ -8,26 +8,40 @@ public class teleporttolabarotorio : MonoBehaviour
 	public GameObject spawn;
 	public Text infoTable;
 	int index;
+    public NAlement[] CAL;
 	private void OnTriggerEnter(Collider other)
 	{
 		Teleport();
 	}
+    private void Start()
+    {
+		Alement(29);
+    }
 
-	public void Alement(int ind, GameObject alement, string info)
+    public void Alement(int ind)
 	{
-		infoTable.text = info;
-		index = ind;
-		Instantiate(alement, spawn.transform);
+		foreach (NAlement ele in CAL)
+		{
+			if (ele.indeex == ind)
+			{
+				infoTable.text = ele.info;
+				index = ele.indeex;
+				infoTable.transform.parent.gameObject.SetActive(true);
+				Instantiate(ele.al, spawn.transform);
+			}
+		}
 	}
 
 	public void Teleport()
-    {
-        SceneManager.LoadScene(index);
-    }
-	
-	public void OnInfo(bool b)
 	{
-		b = !b;
-		infoTable.gameObject.SetActive(b);
+		SceneManager.LoadScene(index);
 	}
 }
+[System.Serializable]
+public class NAlement
+{
+    public int indeex;
+    public string info;
+    public GameObject al;
+}
+
